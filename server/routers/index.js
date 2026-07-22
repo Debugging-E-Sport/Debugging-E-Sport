f39cf715fa7238892fbab524d59b02355f05fce8
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-07-23 04:45:20 +0700

    fix: register snippet router and correct startRoom route prefix and method

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const auth = require("./auth");
 const room = require("./room");
+const snippet = require("./snippet");
 
 router.use(auth);
 router.use(room);
+router.use(snippet);
 
 module.exports = router;
--- server/routers/room.js
+++ server/routers/room.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post("/api/rooms", authentication, RoomController.createRoom);
 router.get("/api/rooms/:code", RoomController.getRoomByCode);
 router.post("/api/rooms/:code/join", authentication, RoomController.joinRoom);
-router.patch("/rooms/:code/start", authentication, RoomController.startRoom);
+router.post("/api/rooms/:code/start", authentication, RoomController.startRoom);
 
 module.exports = router;
