48f3f6dd66f082ff3afe3c2f2b5b8183384e925e
malthafkiram <malthafkiram@gmail.com>
2026-07-22 11:30:22 +0700

    feat: login done

---
+++ server/controllers/authController.js
@@ -0,0 +1,43 @@
+const { where } = require("sequelize");
+const { User } = require("../models/index");
+const { signToken } = require("../helpers/jwt");
+const { chek } = require("../helpers/bycprt");
+class AuthController {
+  static async register(req, res, next) {
+    try {
+      const { username, password } = req.body;
+
+      const user = await User.create({ username, password });
+
+      res.status(201).json({
+        message: "register has been successfuly",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async login(req, res, next) {
+    try {
+      const { username, password } = req.body;
+      if (!username || !password) throw { name: "userNotNull" };
+
+      const user = await User.findOne({ where: { username: username } });
+      if (!user) throw { name: "userNotFound" };
+      if (!chek(password, user.password)) throw { name: "userNotFound" };
+
+      const payload = {
+        id: user.id,
+        username: user.username,
+      };
+
+      const access_token = signToken(payload);
+
+      res.status(200).json(access_token);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = AuthController;
---
+++ server/middlewares/errorHandler.js
@@ -0,0 +1,26 @@
+function errorHandler(error, req, res, next) {
+  console.log(error);
+  let message = "Internal Server Error";
+  let status = 500;
+
+  if (error.name === "userNotNull") {
+    message = "username or password cannot be empty.";
+    status = 400;
+  }
+
+  if (error.name === "SequelizeValidationError") {
+    message = error.errors[0].message;
+    status = 400;
+  }
+
+  if (error.name === "userNotFound") {
+    message = "user not found";
+    status = 403;
+  }
+
+  res.status(status).json({
+    message,
+  });
+}
+
+module.exports = errorHandler;
