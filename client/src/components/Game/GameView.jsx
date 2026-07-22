3c200c76488d370fa7132087e3d3e7e725d65ae4
RESKY SAPUTRA <dopevwxyz@gmail.com>
2026-07-23 05:16:25 +0700

    fix

--- client/src/components/Game/GameView.jsx
+++ client/src/components/Game/GameView.jsx
@@ -2,6 +2,7 @@ import GameHeader from './GameHeader'
 import CodeSnippetPanel from './CodeSnippetPanel'
 import AnswerPanel from './AnswerPanel'
 import LiveLeaderboard from './LiveLeaderboard'
+import { ErrorBoundary } from '../ErrorBoundary'
 
 export default function GameView({ roomCode, user }) {
   return (
@@ -9,9 +10,11 @@ export default function GameView({ roomCode, user }) {
       <GameHeader roomCode={roomCode} user={user} />
       
       <main className="max-w-[1440px] w-full mx-auto px-4 py-5 grid grid-cols-12 gap-5 flex-1">
-        <CodeSnippetPanel />
-        <AnswerPanel />
-        <LiveLeaderboard />
+        <ErrorBoundary>
+          <CodeSnippetPanel />
+          <AnswerPanel />
+          <LiveLeaderboard />
+        </ErrorBoundary>
       </main>
     </div>
   )
